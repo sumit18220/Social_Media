931152259525dfc418596e0b1a280263ed4fed44
Bhu-1 <bhuvanchandra37@gmail.com>
2018-12-07 23:52:48 +0530

    added login and resgistration page

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -13,6 +13,7 @@ class Home extends CI_Controller {
     }
     
     public function registration(){
+        
         $this->load->view("registration");
     }
     
--- application/views/login.php
+++ application/views/login.php
@@ -1,8 +1,55 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="/Social_Media/static/css/bootstrap.css" type="text/css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div class="jumbotron jumbotron-fluid">
+        	<div class="container">
+        		<div class="row">
+        			<div class="col-sm-offset-2">
+        				<img src="/Social_Media/static/img/ungineering_logo.svg">
+        			</div>
+        			<div class="col-sm-10">
+    					<h1 class="display-3">Ungineering Social Account</h1>
+        			</div>
+        		</div>
+        	</div>
+        </div>
+        <div class="container">
+        	<div class="row">
+        		<div class="col-sm">
+        			<h3 style="text-align: center;">Log In to Ungineering Social Account</h3>
+        		</div>
+        	</div>
+    		<div class="container">
+    			<form action="/action_page.php">
+					  <div class="form-group">
+					    <label for="email">Email address:</label>
+					    <input type="email" class="form-control" id="email">
+					  </div>
+					  <div class="form-group">
+					    <label for="pwd">Password:</label>
+					    <input type="password" class="form-control" id="pwd">
+					  </div>
+					  <div class="form-group form-check">
+					    <label class="form-check-label">
+					      <input class="form-check-input" type="checkbox"> Remember me
+					    </label>
+					  </div>
+				  <button type="submit" class="btn btn-primary">Submit</button>
+				</form>
+    		</div>
+    		<div>
+    			<p style="text-align: right;">New User..?<a href="signup.php">Resgiter</a>Here</p>
+    		</div>
+        </div>
+    </body>
+</html>
 
